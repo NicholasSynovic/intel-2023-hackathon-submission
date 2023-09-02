28558186aad61f3d938d734e9e2310f893180c93
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-09-02 13:29:57 -0500

    Re-add SQL schemas

---
+++ hackathon_submission/createDB.py
@@ -0,0 +1,16 @@
+from hackathon_submission.schemas.sql import SQL
+from pathlib import Path
+from pandas import DataFrame
+
+def main()  ->  None:
+    dbPath: Path = Path("storage/data.db")
+    sql: SQL = SQL(sqliteDBPath=dbPath)
+
+    df: DataFrame = DataFrame(data={"Username": ["user"], "Password": ["password"]})
+
+    sql.createSchema_Users()
+    sql.writeDFToDB(df=df, tableName="Users", keepIndex=True)
+    sql.closeConnection()
+
+if __name__ == "__main__":
+    main()
