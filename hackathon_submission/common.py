623beaf6f0988165531448ddc730256dd5f7b023
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-09-03 14:03:43 -0500

    Fix type errors

--- hackathon_submission/common.py
+++ hackathon_submission/common.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
-SITE_STATE: dict[str, str] = {
+SITE_STATE = {
     "page_title": "Empire General Hospital Patient Portal",
     "initial_sidebar_state": "collapsed",
     "layout": "wide",
 }
 
-HIDDEN_SIDEBAR_CSS: dict[str, str] = {
+HIDDEN_SIDEBAR_CSS = {
     "body": """
 <style>
     [data-testid="collapsedControl"] {
@@ -22,7 +22,7 @@ DB_PATH: Path = Path("storage/data.db")
 MODEL_PATH: Path = Path("model")
 
 PAGE_HEADER: str = """# Empire General Hospital Patient Self-Diagnosis Tool
-> A prototype developed by Nicholas M. Synovic ((nsynovic@luc.edu)[mailto:nsynovic@luc.edu])"""
+> A prototype developed by Nicholas M. Synovic ([nsynovic@luc.edu](mailto:nsynovic@luc.edu))"""
 
 PAGE_FOOTER: str = """To speak to a medical professional at Empire General Hospital, call `(xxx) xxx-xxxx`\n
 For all medical emergencies, call `911`"""
