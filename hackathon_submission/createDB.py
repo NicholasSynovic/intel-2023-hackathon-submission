c81a29b8dc806bfc74b7370da3d36b79bb332ada
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-09-03 14:04:24 -0500

    Use the common components

--- hackathon_submission/createDB.py
+++ hackathon_submission/createDB.py
@@ -1,13 +1,11 @@
-from pathlib import Path
-
 from pandas import DataFrame
 
+from hackathon_submission import common
 from hackathon_submission.schemas.sql import SQL
 
 
 def main() -> None:
-    dbPath: Path = Path("storage/data.db")
-    sql: SQL = SQL(sqliteDBPath=dbPath)
+    sql: SQL = SQL(sqliteDBPath=common.DB_PATH)
 
     df: DataFrame = DataFrame(data={"Username": ["user"], "Password": ["password"]})
 
