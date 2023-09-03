75d1a76ca81cfc540fd8fc00b11f25938ee8c42b
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-09-03 14:32:34 -0500

    Add common components

--- hackathon_submission/pages/5_report.py
+++ hackathon_submission/pages/5_report.py
@@ -1,26 +1,22 @@
 import streamlit as st
-from hackathon_submission.conf import hideSidebarCSS, pageState
 from streamlit_extras.switch_page_button import switch_page
+from hackathon_submission import common
+from pandas import DataFrame
 
-HEADER: str = f"""# Empire General Hospital Patient Portal
-> A prototype developed by Nicholas M. Synovic
+MESSAGE: str = f"""## {st.session_state["username"]}'s Report
 
-## {st.session_state["username"]}'s Report
-
-Below are your *estimated* prognoses with their probabilities.
+Below are your estimated prognoses with their probabilities.
 Please know that these are only estimations by an automated system.
 No medical professional has verified the legitamacy of these prognoses.
-
-To talk to a professional at Empire General Hospital, call `(xxx) xxx-xxxx`
-For medical emergencies, call `911`
 """
 
 
 def main() -> None:
-    st.set_page_config(**pageState)
-    st.markdown(**hideSidebarCSS)
+    st.set_page_config(**common.SITE_STATE)
+    st.markdown(**common.HIDDEN_SIDEBAR_CSS)
 
-    st.write(HEADER)
+    st.write(common.PAGE_HEADER)
+    st.write(MESSAGE)
 
     st.dataframe(
         data=st.session_state["report"],
@@ -28,18 +24,26 @@ def main() -> None:
         hide_index=True,
     )
 
-    col1, col2 = st.columns(spec=[6, 2], gap="large")
+    col1, col2, col3 = st.columns(spec=[4, 1, 1], gap="small")
 
     with col1:
+        logoutButton = st.button(label="Logout")
+        if logoutButton:
+            st.session_state["username"] = ""
+            st.session_state["report"] = DataFrame()
+            switch_page(page_name="about")
+
+    with col2:
         reportSymptomsButton = st.button(label="Report Symptoms")
         if reportSymptomsButton:
             switch_page(page_name="symptoms")
 
-    with col2:
+    with col3:
         aiDoctor = st.button(label="Talk to an AI Doctor")
         if aiDoctor:
             switch_page(page_name="Talk")
 
+    st.write(common.PAGE_FOOTER)
     st.divider()
 
 
