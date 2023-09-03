bdd5ca0431dce641a353f58e9da3689a1f2eaf7f
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-09-03 13:44:14 -0500

    Create common components file

--- hackathon_submission/common.py
+++ hackathon_submission/common.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
-pageState = {
+SITE_STATE: dict[str, str] = {
     "page_title": "Empire General Hospital Patient Portal",
     "initial_sidebar_state": "collapsed",
     "layout": "wide",
 }
 
-hideSidebarCSS = {
+HIDDEN_SIDEBAR_CSS: dict[str, str] = {
     "body": """
 <style>
     [data-testid="collapsedControl"] {
@@ -16,4 +16,13 @@ hideSidebarCSS = {
 """,
     "unsafe_allow_html": True,
 }
-dbPath: Path = Path("storage/data.db")
+
+DB_PATH: Path = Path("storage/data.db")
+
+MODEL_PATH: Path = Path("model")
+
+PAGE_HEADER: str = """# Empire General Hospital Patient Self-Diagnosis Tool
+> A prototype developed by Nicholas M. Synovic ((nsynovic@luc.edu)[mailto:nsynovic@luc.edu])"""
+
+PAGE_FOOTER: str = """To speak to a medical professional at Empire General Hospital, call `(xxx) xxx-xxxx`\n
+For all medical emergencies, call `911`"""
