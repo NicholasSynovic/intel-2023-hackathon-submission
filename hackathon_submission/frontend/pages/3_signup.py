72e9a6370cb28b2473108e052240c830adae1c9b
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-09-03 19:49:25 -0500

    Implement signup via API

--- hackathon_submission/frontend/pages/3_signup.py
+++ hackathon_submission/frontend/pages/3_signup.py
@@ -1,48 +1,11 @@
-from pathlib import Path
-
-import pandas
 import streamlit as st
 from hackathon_submission import common
-from hackathon_submission.schemas.sql import SQL
-from pandas import DataFrame
 from streamlit_extras.switch_page_button import switch_page
+from hackathon_submission.utils import api
 
 MESSAGE: str = "## Sign Up"
 
-LOGIN_ERROR: str = ":red[Invalid {}]"
-
-
-def searchForUser(username: str) -> bool:
-    # MOVE TO BACKEND
-    sql: SQL = SQL(sqliteDBPath=common.DB_PATH)
-    df: DataFrame = pandas.read_sql_table(table_name="Users", con=sql.conn)
-    sql.closeConnection()
-
-    row: DataFrame = df[df["Username"] == username]
-
-    if row.shape[0] > 0:
-        return True
-    return False
-
-
-def createAccount(username: str, password: str) -> bool:
-    # MOVE TO BACKEND
-    sql: SQL = SQL(sqliteDBPath=common.DB_PATH)
-    df: DataFrame = pandas.read_sql_table(table_name="Users", con=sql.conn)
-
-    df.set_index("index", inplace=True)
-    df = df.append(
-        other={"Username": username, "Password": password},
-        ignore_index=True,
-    )
-
-    sql.writeDFToDB(df=df, tableName="Users", keepIndex=True)
-    sql.closeConnection()
-
-
 def main() -> None:
-    LOGIN_SUCCESS: bool = False
-
     st.set_page_config(**common.SITE_STATE)
     st.markdown(**common.HIDDEN_SIDEBAR_CSS)
 
@@ -73,16 +36,12 @@ def main() -> None:
         createAccountButton: bool = st.button(label="Create Account")
 
     if createAccountButton:
-        # REPLACE WITH ASYNC CODE FROM BACKEND
-        if searchForUser(username=username):
-            st.write(LOGIN_ERROR.format("Username"))
+        signup: bool= api.signup(username=username, password=password)
+        if signup:
+            st.session_state["username"] = signup
+            switch_page(page_name="symptoms")
         else:
-            LOGIN_SUCCESS = True
-
-    if LOGIN_SUCCESS:
-        createAccount(username=username, password=password)
-        st.session_state["username"] = username
-        switch_page(page_name="symptoms")
+            st.write(common.ACCOUNT_ERROR_MESSAGE.format("Account Credentials"))
 
     st.divider()
     st.write(common.PAGE_FOOTER)
