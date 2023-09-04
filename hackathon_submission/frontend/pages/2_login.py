b5b44b71c0aa1bf83d2c267d5afe59af9d856591
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-09-03 19:20:14 -0500

    Update page to use the login API

--- hackathon_submission/frontend/pages/2_login.py
+++ hackathon_submission/frontend/pages/2_login.py
@@ -1,8 +1,5 @@
-import pandas
 import streamlit as st
 from hackathon_submission import common
-from hackathon_submission.schemas.sql import SQL
-from pandas import DataFrame
 from streamlit_extras.switch_page_button import switch_page
 from hackathon_submission.utils import api
 
@@ -12,41 +9,7 @@ Test username: `user`\n
 Test password: `password`
 """
 
-def searchForUser(username: str) -> bool:
-    # MOVE TO BACKEND
-    sql: SQL = SQL(sqliteDBPath=common.DB_PATH)
-    df: DataFrame = pandas.read_sql_table(table_name="Users", con=sql.conn)
-    sql.closeConnection()
-
-    row: DataFrame = df[df["Username"] == username]
-
-    if row.shape[0] > 0:
-        return True
-    return False
-
-
-def checkPassword(username: str, password: str) -> bool:
-    # MOVE TO BACKEND
-    sql: SQL = SQL(sqliteDBPath=common.DB_PATH)
-    df: DataFrame = pandas.read_sql_table(table_name="Users", con=sql.conn)
-    sql.closeConnection()
-
-    row: DataFrame = df[df["Username"] == username]
-
-    if row.shape[0] > 1:
-        return False
-
-    try:
-        if row["Password"].to_list()[0] == password:
-            return True
-    except ValueError:
-        return False
-    return False
-
-
 def main() -> None:
-    LOGIN_SUCCESS: bool = False
-
     st.set_page_config(**common.SITE_STATE)
     st.markdown(**common.HIDDEN_SIDEBAR_CSS)
 
@@ -83,19 +46,12 @@ def main() -> None:
             loginButton: bool = st.button(label="Login")
 
         if loginButton:
-            # REPLACE WITH ASYNC CODE FROM BACKEND
-            if searchForUser(username=username):
-                if checkPassword(username=username, password=password):
-                    LOGIN_SUCCESS = True
-                else:
-                    st.write(common.ACCOUNT_ERROR_MESSAGE.format("Password"))
+            if api.login(username=username, password=password):
+                st.session_state["username"] = username
+                switch_page(page_name="symptoms")
             else:
-                st.write(common.ACCOUNT_ERROR_MESSAGE.format("Username"))
-
-        if LOGIN_SUCCESS:
-            st.session_state["username"] = username
-            switch_page(page_name="symptoms")
-
+                st.write(common.ACCOUNT_ERROR_MESSAGE.format("Account Credentials"))
+    
     else:
         st.write(common.SERVER_ERROR_MESSAGE)
 
