4b7e826b55397f96f68626a22bc37bd860274122
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-09-03 19:49:13 -0500

    Implement signup middleware

--- hackathon_submission/utils/api.py
+++ hackathon_submission/utils/api.py
@@ -3,6 +3,15 @@ from requests.exceptions import ConnectionError
 
 URL: str = "http://localhost:8000"
 
+def str2bool(data: str)  ->  bool:
+    if data == "true":
+        return True
+    elif data == "false":
+        return False
+    else:
+        return data
+
+
 def check() ->  bool:
     try:
         resp: Response = get(url=URL)
@@ -13,9 +22,14 @@ def check() ->  bool:
 def login(username: str, password: str = "test")    ->  bool:
     resp: Response = post(url=f"{URL}/api/account/login?username={username}&password={password}")
     data: str = resp.content.decode()
+    return str2bool(data=data)
 
-    if data == "true":
-        return True
-    return False
-
-print(login(username="user", password="password"))
+def logout()    ->  bool:
+    resp: Response = post(url=f"{URL}/api/account/logout")
+    data: str = resp.content.decode()
+    return str2bool(data=data)
+    
+def signup(username: str, password: str)    ->  bool:
+    resp: Response = post(url=f"{URL}/api/account/signup?username={username}&password={password}")
+    data: str = resp.json()["username"]
+    return str2bool(data=data)
