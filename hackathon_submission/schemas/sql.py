b99e55bd389a38e06bad08d1ac680c7a53a301cc
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-09-04 12:47:42 -0500

    Create Reports table schema

--- hackathon_submission/schemas/sql.py
+++ hackathon_submission/schemas/sql.py
@@ -12,6 +12,7 @@ class SQL:
         self.sqliteURI: str = f"sqlite:///{sqliteDBPath.absolute().__str__()}"
 
         self.usersTableName: str = "Users"
+        self.reportsTable: str = "Reports"
 
         self.metadata: MetaData = MetaData()
 
@@ -37,13 +38,82 @@ class SQL:
             Column(name="Password", type_=String),
         )
 
+    def createSchema_Reports(self) -> None:
+        Table(
+            self.reportsTable,
+            self.metadata,
+            Column(
+                name="ID",
+                type_=Integer,
+                primary_key=True,
+                unique=True,
+                autoincrement=True,
+            ),
+            Column(
+                name="Username",
+                type_=String,
+            ),
+            Column(
+                name="Report Time",
+                type_=Integer,
+            ),
+            Column(
+                name="Symptoms",
+                type_=String,
+            ),
+            Column(
+                name="Symptoms",
+                type_=String,
+            ),
+            Column(
+                name="Prognosis 1",
+                type_=String,
+            ),
+            Column(
+                name="Probability 1",
+                type_=String,
+            ),
+            Column(
+                name="Prognosis 2",
+                type_=String,
+            ),
+            Column(
+                name="Probability 2",
+                type_=String,
+            ),
+            Column(
+                name="Prognosis 3",
+                type_=String,
+            ),
+            Column(
+                name="Probability 3",
+                type_=String,
+            ),
+            Column(
+                name="Prognosis 4",
+                type_=String,
+            ),
+            Column(
+                name="Probability 4",
+                type_=String,
+            ),
+            Column(
+                name="Prognosis 5",
+                type_=String,
+            ),
+            Column(
+                name="Probability 5",
+                type_=String,
+            ),
+        )
+
     def createTables(self) -> None:
         self.metadata.create_all(bind=self.conn)
 
     def writeDFToDB(
         self,
         df: DataFrame,
-        tableName: Literal["Users"],
+        tableName: Literal["Users", "Reports"],
         keepIndex: bool,
         indexColumn: None = None,
     ) -> None:
