ac71033669cfe1fa089f93dc2803a6a7ae9262af
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-09-03 19:48:58 -0500

    Implement signup endpoint

--- hackathon_submission/backend/server.py
+++ hackathon_submission/backend/server.py
@@ -1,69 +1,91 @@
+from typing import Any, Literal
+
+import pandas
 from fastapi import FastAPI
 from hackathon_submission import common
 from hackathon_submission.schemas.sql import SQL
 from pandas import DataFrame
-import pandas
-from typing import Any, Literal
 
 app: FastAPI = FastAPI()
 
-def getUsersTable() ->  DataFrame:
+
+def getUsersTable() -> DataFrame:
     sql: SQL = SQL(sqliteDBPath=common.DB_PATH)
     df: DataFrame = pandas.read_sql_table(table_name="Users", con=sql.conn)
     sql.closeConnection()
     return df
 
-def usernameExists(username: str, df: DataFrame)   ->  bool:
+
+def usernameExists(username: str, df: DataFrame) -> bool:
     return username in df["Username"].values
 
-def checkPassword(username: str, password: str, df: DataFrame)  ->  bool:
+
+def checkPassword(username: str, password: str, df: DataFrame) -> bool:
     row: Any = df[df["Username"] == username]
-    
+
     if row["Password"].to_list()[0] == password:
         return True
     return False
 
 
 @app.get(path="/")
-def check() ->  Literal[True]:
+def check() -> Literal[True]:
     return True
 
+
 @app.post(path="/api/account/login")
-def login(username: str, password: str) ->  bool:
-    username=username.lower()
+def login(username: str, password: str) -> bool:
+    username = username.lower()
     df: DataFrame = getUsersTable()
-    
+
     userExists: bool = usernameExists(username=username, df=df)
     if userExists:
         return checkPassword(username=username, password=password, df=df)
     return False
 
+
 @app.post(path="/api/account/logout")
-def logout()    ->  Literal[True]:
+def logout() -> Literal[True]:
     return True
 
+
 @app.post(path="/api/account/signup")
-def signup(username: str, password: str)    ->  bool:
-    username=username.lower()
+def signup(username: str, password: str) -> dict:
+    username = username.lower()
+    sql: SQL = SQL(sqliteDBPath=common.DB_PATH)
     df: DataFrame = getUsersTable()
-    
+
     userExists: bool = usernameExists(username=username, df=df)
     if userExists:
-        return False
+        return {"username": False}
+
+    df.set_index("index", inplace=True)
+    df = df.append(
+        other={"Username": username, "Password": password},
+        ignore_index=True,
+    )
+
+    sql.writeDFToDB(df=df, tableName="Users", keepIndex=True)
+    sql.closeConnection()
+
+    return {"username": username}
+
 
 @app.post(path="/api/inference/prognosis")
-def inferencePrognosis()    ->  bool:
+def inferencePrognosis() -> bool:
     pass
+
 
 @app.get(path="/api/storage/symptoms")
-def getSymptoms()   ->  bool:
+def getSymptoms() -> bool:
     pass
+
 
 @app.get(path="/api/storage/prognosis")
-def getPrognosis()  ->  bool:
+def getPrognosis() -> bool:
     pass
+
 
 @app.post(path="/api/generate/report")
-def generateReport()    ->  bool:
+def generateReport() -> bool:
     pass
-
