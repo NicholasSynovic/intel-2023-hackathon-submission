7f9a7c851767ab78e90772a68d86bca9852a3079
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-09-04 21:28:56 -0500

    Add overview to symptom summary

--- hackathon_submission/frontend/pages/4_report.py
+++ hackathon_submission/frontend/pages/4_report.py
@@ -49,6 +49,23 @@ def main() -> None:
                 f"### Report From {datetime.utcfromtimestamp(df['time']).strftime('%A, %B %d %Y @ %I:%M %p')}",
             )
             st.write(f"**Symptoms**: {df['symptoms']}")
+
+            topProgProb: list = df["df"].iloc[0][["Prognosis", "Probability"]].to_list()
+
+            prob: float = float(topProgProb[1][0:3])
+            if prob <= 30:
+                st.write(
+                    f"\n**Overview**: :green[Good news! You most likely do not have {topProgProb[0]}. If your sympyoms worsen, please seek medical attention.]"
+                )
+            elif (prob > 30) and (prob < 50):
+                st.write(
+                    f"\n**Overview**: :yellow[You are at moderate risk for {topProgProb[0]}. If your sympyoms worsen, please seek medical attention.]"
+                )
+            else:
+                st.write(
+                    f"\n**Overview**: :red[You are at risk for {topProgProb[0]}. Please seek medical attention.]"
+                )
+
             st.dataframe(data=df["df"], use_container_width=True, hide_index=True)
             deleteReportButton = st.button(label="Delete Report")
             if deleteReportButton:
